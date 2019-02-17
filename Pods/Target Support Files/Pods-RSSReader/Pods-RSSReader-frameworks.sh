c5e5a8a167a2db7d8bf3e557587a8e01de58a129
papin.alex <papin.alex@sharp-dev.net>
2019-02-18 01:18:57 +0400

    removed unused frameworks

--- Pods/Target Support Files/Pods-RSSReader/Pods-RSSReader-frameworks.sh
+++ Pods/Target Support Files/Pods-RSSReader/Pods-RSSReader-frameworks.sh
@@ -153,20 +153,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FeedKit/FeedKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReSwift/ReSwift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/ReSwiftRouter/ReSwiftRouter.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FeedKit/FeedKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReSwift/ReSwift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/ReSwiftRouter/ReSwiftRouter.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
